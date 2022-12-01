c72ffd6732ee2c75cf280f366c1a0eca3037647b
Tienlong Pham <tienlong.pham@slalom.com>
2022-12-01 14:44:33 -0600

    Pre-process day01 input, add timing and links

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,48 +1,43 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
-
-const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-  const sums = input
+const parseInput = (rawInput: string) => rawInput
     .split("\n\n")
     .map((group) => group.split("\n"))
     .map((group) => group.reduce((acc, calorie) => acc + Number(calorie), 0));
-  return Math.max(...sums);
+
+const part1 = (rawInput: string) => {
+  return Math.max(...parseInput(rawInput));
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-  const sums = input
-    .split("\n\n")
-    .map((group) => group.split("\n"))
-    .map((group) => group.reduce((acc, calorie) => acc + Number(calorie), 0));
-  return sums
-    .sort()
+  return parseInput(rawInput)
+    .sort((first, second) => (first > second ? -1 : 1))
     .slice(0, 3)
-    .reduce((acc, calories) => acc + calories, 0);
+    .reduce((acc, calorie) => acc += calorie, 0);
 };
+
+const testInput = `
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+`
 
 run({
   part1: {
     tests: [
       {
-        input: `
-        1000
-        2000
-        3000
-
-        4000
-
-        5000
-        6000
-
-        7000
-        8000
-        9000
-
-        10000
-        `,
+        input: testInput,
         expected: 24000,
       },
     ],
@@ -51,22 +46,7 @@ run({
   part2: {
     tests: [
       {
-        input: `
-        1000
-        2000
-        3000
-
-        4000
-
-        5000
-        6000
-
-        7000
-        8000
-        9000
-
-        10000
-        `,
+        input: testInput,
         expected: 45000,
       },
     ],
