0a62ea1c852820c53b3aab49bf259120b1ac5c77
Tienlong Pham <10876966+aglorei@users.noreply.github.com>
2022-12-03 10:11:06 -0600

    Apply generic getCommonItem to both day03 parts

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -2,6 +2,15 @@ import run from "aocrunner";
 
 const parseInput = (rawInput: string) => rawInput.split("\n");
 
+const getCommonItem = (...rucksacks: string[]) => {
+  const rucksackSets = rucksacks.map((rucksack) => new Set(rucksack));
+  return [
+    ...rucksackSets.reduce((acc, rucksack) => {
+      return new Set([...acc].filter((item) => rucksack.has(item)));
+    }),
+  ].join("");
+};
+
 const getItemPriority = (item: string) => {
   const modifier = item == item.toUpperCase() ? 38 : 96;
   return item.charCodeAt(0) - modifier;
@@ -10,11 +19,7 @@ const getItemPriority = (item: string) => {
 const part1 = (rawInput: string) => {
   return parseInput(rawInput).reduce((acc, rucksack) => {
     const half = Math.ceil(rucksack.length / 2);
-    const compartment1 = new Set(rucksack.slice(0, half));
-    const compartment2 = new Set(rucksack.slice(half));
-    const item = [...compartment1]
-      .filter((item) => compartment2.has(item))
-      .join("");
+    const item = getCommonItem(rucksack.slice(0, half), rucksack.slice(half));
     return (acc += getItemPriority(item));
   }, 0);
 };
@@ -23,14 +28,11 @@ const part2 = (rawInput: string) => {
   const rucksacks = parseInput(rawInput);
   let prioritySum = 0;
   for (let i = 0; i < rucksacks.length; i += 3) {
-    const elf1 = new Set(rucksacks[i]);
-    const elf2 = new Set(rucksacks[i + 1]);
-    const elf3 = new Set(rucksacks[i + 2]);
-    const item = [
-      ...[elf1, elf2, elf3].reduce((acc, elf) => {
-        return new Set([...acc].filter((item) => elf.has(item)));
-      }),
-    ].join("");
+    const item = getCommonItem(
+      rucksacks[i],
+      rucksacks[i + 1],
+      rucksacks[i + 2],
+    );
     prioritySum += getItemPriority(item);
   }
   return prioritySum;
