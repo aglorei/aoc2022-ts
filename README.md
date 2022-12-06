8a5193c0669095d2edbf7924bfec9353a5753699
Tienlong Pham <tienlong.pham@slalom.com>
2022-12-06 03:07:15 -0600

    Initialize day06 solution

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
-![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
@@ -127,16 +127,16 @@ Both parts: 21.076875ms
 
 ```
 Day 05
-Time part 1: 0.59ms
-Time part 2: 1.17ms
-Both parts: 1.759792ms
+Time part 1: 0.574ms
+Time part 2: 1.142ms
+Both parts: 1.716875ms
 ```
 
 ```
 Day 06
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.274ms
+Time part 2: 2.092ms
+Both parts: 2.3665000000000003ms
 ```
 
 ```
@@ -273,8 +273,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 10/50
-Total time: 26.224ms
+Total stars: 12/50
+Total time: 28.548ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day06/index.ts
@@ -0,0 +1,74 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+const charactersProcessed = (rawInput: string, windowSize: number) => {
+  const input = parseInput(rawInput);
+  for (let i = windowSize - 1; i < input.length; i++) {
+    const sequence = input.slice(i - windowSize + 1, i + 1).split("");
+    if (new Set(sequence).size == sequence.length) return i + 1;
+  }
+};
+
+const part1 = (rawInput: string) => {
+  return charactersProcessed(rawInput, 4);
+};
+
+const part2 = (rawInput: string) => {
+  return charactersProcessed(rawInput, 14);
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `mjqjpqmgbljsphdztnvjfqwrcgsmlb`,
+        expected: 7,
+      },
+      {
+        input: `bvwbjplbgvbhsrlpgdmjqwftvncz`,
+        expected: 5,
+      },
+      {
+        input: `nppdvjthqldpwncqszvftbrmjlhg`,
+        expected: 6,
+      },
+      {
+        input: `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`,
+        expected: 10,
+      },
+      {
+        input: `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`,
+        expected: 11,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `mjqjpqmgbljsphdztnvjfqwrcgsmlb`,
+        expected: 19,
+      },
+      {
+        input: `bvwbjplbgvbhsrlpgdmjqwftvncz`,
+        expected: 23,
+      },
+      {
+        input: `nppdvjthqldpwncqszvftbrmjlhg`,
+        expected: 23,
+      },
+      {
+        input: `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`,
+        expected: 29,
+      },
+      {
+        input: `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`,
+        expected: 26,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
